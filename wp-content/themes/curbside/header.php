963e330cf345e8938212d0eb532e4f5a8c14289c
Spencer Finnell <spencerfinnell@gmail.com>
2014-04-24 09:21:04 -0400

    Apple touch icons.

--- wp-content/themes/curbside/header.php
+++ wp-content/themes/curbside/header.php
@@ -19,10 +19,10 @@
 	<meta name="apple-mobile-web-app-capable" content="yes">
 	<meta name="apple-mobile-web-app-status-bar-style" content="black">
 
-	<link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/images/touch-icon-iphone-60x60-precomposed.png">
-	<link rel="apple-touch-icon" sizes="60x60" href="<?php echo get_template_directory_uri(); ?>/images/touch-icon-ipad-76x76-precomposed.png">
-	<link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/images/touch-icon-iphone-retina-120x120-precomposed.png">
-	<link rel="apple-touch-icon" sizes="144x144" href="<?php echo get_template_directory_uri(); ?>/images/touch-icon-ipad-retina-152x152-precomposed.png">
+	<link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/images/apple-touch-icon-60x60.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="<?php echo get_template_directory_uri(); ?>/images/apple-touch-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="<?php echo get_template_directory_uri(); ?>/images/apple-touch-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="<?php echo get_template_directory_uri(); ?>/images/apple-touch-icon-152x152.png">
 
 	<title><?php wp_title( ' - ', true, 'right' ); ?></title>
 
