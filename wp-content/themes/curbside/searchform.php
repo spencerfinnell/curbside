e92d1432f39b59288897a532997276f4770f0f29
Spencer Finnell <spencerfinnell@gmail.com>
2014-04-24 08:26:53 -0400

    Limit search to truck names.

--- wp-content/themes/curbside/searchform.php
+++ wp-content/themes/curbside/searchform.php
@@ -1,3 +1,4 @@
 <form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
 	<input type="search" name="s" id="s" placeholder="Search" value="<?php echo get_search_query(); ?>">
+	<input type="hidden" name="post_type" value="truck" />
 </form>
